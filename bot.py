14d26b50deaf23612cd4b72ed7174912ecdb624b
vaniasetia <vanias2304@gmail.com>
2024-05-26 21:08:23 +0530

    Add book finding feature and Google Books API

--- bot.py
+++ bot.py
@@ -31,6 +31,54 @@ class BookBot(commands.Bot):
 
         await self.process_commands(message)
 
+    async def book_info(self, ctx, *args):
+        if not args:
+            await ctx.send("Please provide the book author's name.")
+            return
+
+        query = '+'.join(args)
+        url = f'https://www.googleapis.com/books/v1/volumes?q={query}&key={self.api_key}'
+        response = requests.get(url)
+        books = response.json().get('items', [])
+
+        if not books:
+            await ctx.send("No books found.")
+            return
+
+        for i, book in enumerate(books, 1):
+            volume_info = book['volumeInfo']
+            title = volume_info.get('title', 'No title available')
+            authors = volume_info.get('authors', ['No author available'])
+            authors_str = ", ".join(authors)
+            await ctx.send(f'{i}. [Title]: {title}\n\t[Author]: {authors_str}')
+
+        await ctx.send('Choose one, enter -1 to stop')
+
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+
+        try:
+            msg = await self.wait_for('message', check=check, timeout=60.0)
+        except asyncio.TimeoutError:
+            await ctx.send('No response, search stopped.')
+            return
+
+        selection = int(msg.content)
+        if selection == -1:
+            await ctx.send('Search stopped.')
+            return
+
+        if 1 <= selection <= len(books):
+            selected_book = books[selection - 1]
+            volume_info = selected_book['volumeInfo']
+            title = volume_info.get('title', 'No title available')
+            authors = volume_info.get('authors', ['No author available'])
+            description = volume_info.get('description', 'No description available')
+            average_rating = volume_info.get('averageRating', 'No rating available')
+            authors_str = ", ".join(authors)
+            await ctx.send(f'[Title]: {title}\n[Author]: {authors_str}\n[Description]: {description}\n[Average Rating]: {average_rating}')
+        else:
+            await ctx.send('Invalid selection.')
 
 # Create bot instance with necessary intents
 intents = discord.Intents.default()
